37c41bb66f2fb39a1189a7cb6002e2aa763f4a9e
icecream17 <nguyeste008@students.garlandisd.net>
2023-05-25 07:06:36 -0500

    realize that indexing is impossible

--- js/nestarr.js
+++ js/nestarr.js
@@ -3,21 +3,6 @@ const assert = (condition, message) => {
 }
 
 /**
- * Makes a class callable
- *
- * @example
- * ```js
- * makeCallable(class{})
- * ```
- */
-const makeCallable = c =>
-   new Proxy(c, {
-      apply (target, thisArg, argArray: A) {
-         return new target(...argArray)
-      }
-   })
-
-/**
  * https://tc39.es/ecma262/multipage/ordinary-and-exotic-objects-behaviours.html#sec-getprototypefromconstructor
  */
 const getPrototypeFromConstructor = (constructor_, intrinsicDefaultProto) => {
@@ -26,31 +11,15 @@ const getPrototypeFromConstructor = (constructor_, intrinsicDefaultProto) => {
   return protoIsObject ? proto : intrinsicDefaultProto
 }
 
-/**
- * https://tc39.es/ecma262/multipage/ordinary-and-exotic-objects-behaviours.html#sec-validateandapplypropertydescriptor
- */
-const validateAndApplyPropertyDescriptor = (o, p, extensible, desc, current) => {
-  assert(typeof p === "string" || typeof p === "symbol", "NestArray set property internal error: argument was not a PropertyKey")
-  if (current === undefined) {
-    if (extensible === false) return false;
-    if (o === undefined) return true;
-    
-  }
-}
-
-/**
- * https://tc39.es/ecma262/multipage/ordinary-and-exotic-objects-behaviours.html#sec-ordinarydefineownproperty
- */
-const ordinaryDefineOwnProperty = (o, p, desc) => {
-  const current = Reflect.getOwnPropertyDescriptor(o) // guaranteed to not fail given o is an object
-  const extensible = Reflect.isExtensible(o) // same as Object.isExtensible
-  return validateAndApplyPropertyDescriptor(o, p, extensible, desc, current)
-}
-
 // /**
 //  * https://tc39.es/ecma262/multipage/abstract-operations.html#sec-tonumber
 //  */
 // const toNumber = arg => +arg
+
+const isArrayIndex = prop => {
+  const num = +prop
+  return Number.isInteger(num) && !Object.is(num, -0) && num < 2**32 - 1 && prop === String(num)
+}
 
 /**
  * https://tc39.es/ecma262/multipage/abstract-operations.html#sec-touint32
@@ -96,7 +65,8 @@ const nestArraySetLength = (a, desc) => {
     const succeeded = Reflect.defineProperty(a, "length", newLenDesc)
     if (succeeded === false) return false;
     // I trust that a.[[getOwnPropertyKeys]] is sane and does the same thing as iterating over property keys directly
-    const pIndices = Object.getOwnPropertyNames(a).filter(p => String(+p) === p && toUint32(p) >= newLen).sort((a, b) => +a - +b)
+    // This is actually "toUint32(p)" but the result is the same                  vv
+    const pIndices = Object.getOwnPropertyNames(a).filter(p => isArrayIndex(p) && +p >= newLen).sort((a, b) => +b - +a)
     for (const p of pIndices) {
       const deleteSucceeded = Reflect.deleteProperty(a, p)
       if (deleteSucceeded === false) {
@@ -107,7 +77,7 @@ const nestArraySetLength = (a, desc) => {
       }
     }
     if (newWritable === false) {
-      const succeeded = Reflect.defineProperty("length", { writable: false })
+      const succeeded = Reflect.defineProperty(a, "length", { writable: false })
       assert(succeeded, "NestArray instance internal error: `length`: cannot set writability to false")
     }
     return true
@@ -120,15 +90,37 @@ const nestArrayInstanceProxyHandler = {
   defineProperty(target, property, descriptor) {
     if (property === "length") {
       return nestArraySetLength(target, descriptor)
+    } else if (isArrayIndex(property)) {
+      const lengthDesc = Object.getOwnPropertyDescriptor(target, "length")
+      // Superseded by length integer check
+      // assert("value" in lengthDesc || "writable" in lengthDesc, "NestArray instance internal error: `[index]`: !IsDataDescriptor(lengthDesc)")
+      assert(lengthDesc.configurable === false, "NestArray instance internal error: `[index]`: lengthDesc.configurable must be false")
+      const length = lengthDesc.value
+      // length is a non-negative Integral number -- note that 1/length > 0 is sufficient to check for non-negativity
+      assert(Number.isInteger(length) && 1/length >= 0, "NestArray instance internal error: `[index]`: length is not a nonnegative integer")
+      const index = toUint32(property)[
+      if (index >= length && lengthDesc.writable === false) return false;
+      const succeeded = Reflect.defineProperty(target, property, descriptor)
+      if (succeeded === false) return false;
+      if (index >= length) {
+        lengthDesc.value = index + 1
+        const succeeded = Reflect.defineProperty(a, "length", lengthDesc)
+        assert(succeeded, "NestArray instance internal error: `[index]`: index >= length but cannot change length")
+      }
+      return true
     }
-    // TODO
+    return Reflect.defineProperty(target, property, descriptor)
   }
 }
 
+const ArrayCreate = (length, proto=NestArray.prototype) => {
+  
+}
+
 const nestArrayProxyHandler = {
+  // Allows a class to be called as a function
   apply(target, thisArg, argArray) { return this.construct(target, argArray, thisArg) },
   construct(target, argArray, newTarget) {
-    
     if (target !== newTarget && typeof newTarget === "function") {
       return new newTarget(...argArray)
     }
